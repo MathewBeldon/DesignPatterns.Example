5d2e41a50798106d342980b197de5c8caa754ebe
Matt <mathew.beldon@hotmail.co.uk>
2022-04-04 19:52:38 +0100

    [+] Singleton pattern

---
+++ Creational/Object/Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            var singleton = TheSingleton.Instance;
+
+            //call singleton, will also initialise
+            singleton.DoStuff();
+
+            //call singleton, already initialised
+            singleton.DoStuff();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Creational/Object/Singleton/Singleton/TheSingleton.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton
+{
+    public sealed class TheSingleton
+    {
+        private static readonly Lazy<TheSingleton> _lazySingleton = 
+            new Lazy<TheSingleton>(() => new TheSingleton());
+
+        private TheSingleton()
+        {
+            Console.WriteLine("CREATED A SINGLETON");
+        }
+
+        public static TheSingleton Instance
+        {
+            get { return _lazySingleton.Value; }
+        }
+
+        public void DoStuff()
+        {
+            Console.WriteLine("I AM THE SINGLETON");
+        }
+    }
+}
