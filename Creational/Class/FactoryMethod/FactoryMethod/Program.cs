70e1ac5a490b1ea33489d12a29a7e0b7711309b4
Matt <mathew.beldon@hotmail.co.uk>
2022-04-05 20:05:17 +0100

    [+] Factory Method pattern

---
+++ Creational/Class/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            var productForLower = new ConcreteCreator("lower").Product;
+            var productForUpper = new ConcreteCreator("upper").Product;
+
+            Console.WriteLine(productForLower.DoStuff("TO LOWER"));
+            Console.WriteLine(productForUpper.DoStuff("to upper"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Creational/Class/FactoryMethod/FactoryMethod/TheFactoryMethod.cs
@@ -0,0 +1,56 @@
+﻿namespace FactoryMethod
+{
+    abstract class Product 
+    {
+        public abstract string DoStuff(string stuff);
+    }
+
+    class ConcreteProductToLowerCase : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.ToLower();
+        }
+    }
+
+    class ConcreteProductToUpperCase : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.ToUpper();
+        }
+    }
+
+    abstract class Creator
+    {
+        private Product _product;
+
+        public Creator(string toCase)
+        {
+            _product = CreateProduct(toCase);
+        }
+
+        public Product Product { get { return _product; } }
+        public abstract Product CreateProduct(string toCase);
+    }
+
+    class ConcreteCreator : Creator
+    {
+        public ConcreteCreator(string toCase) : base(toCase)
+        {
+        }
+
+        public override Product CreateProduct(string toCase)
+        {
+            switch (toCase.ToLower())
+            {
+                case "lower":
+                    return new ConcreteProductToLowerCase();
+                case "upper":
+                    return new ConcreteProductToUpperCase();
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+}
