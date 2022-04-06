7c853ab7e21668552bfcac83b3f640da9a456062
Matt <mathew.beldon@hotmail.co.uk>
2022-04-06 21:40:49 +0100

    [+] Abstract Factory Pattern

---
+++ Creational/Object/AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factoryForCase = SuperFactory.CreateConcreateFactory("case");
+            var productForUpper = factoryForCase.GetProduct("upper");
+            var productForLower = factoryForCase.GetProduct("lower");
+
+            Console.WriteLine(productForLower.DoStuff("TO LOWER"));
+            Console.WriteLine(productForUpper.DoStuff("to upper"));
+
+
+            var factoryForReplace = SuperFactory.CreateConcreateFactory("replace");
+            var productForComma = factoryForReplace.GetProduct("comma");
+            var productForSemicolon = factoryForReplace.GetProduct("semicolon");
+
+            Console.WriteLine(productForComma.DoStuff("I want commas"));
+            Console.WriteLine(productForSemicolon.DoStuff("I want semicolons"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Creational/Object/AbstractFactory/AbstractFactory/TheAbstractFactory.cs
@@ -0,0 +1,89 @@
+﻿namespace AbstractFactory
+{
+    abstract class Product
+    {
+        public abstract string DoStuff(string stuff);
+    }
+
+    class ConcreteProductToLowerCase : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.ToLower();
+        }
+    }
+
+    class ConcreteProductToUpperCase : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.ToUpper();
+        }
+    }
+
+    class ConcreteProductSpaceToComma : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.Replace(" ", ",");
+        }
+    }
+
+    class ConcreteProductSpaceToSemicolon : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.Replace(" ", ";");
+        }
+    }
+
+    abstract class SuperFactory
+    {
+        public abstract Product GetProduct(string productType);
+
+        public static SuperFactory CreateConcreateFactory(string concreteType)
+        {
+            switch (concreteType.ToLower())
+            {
+                case "case":
+                    return new ConcreateCreatorCaseFactory();
+                case "replace":
+                    return new ConcreateCreatorReplaceFactory();
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+
+    class ConcreateCreatorCaseFactory : SuperFactory
+    {
+        public override Product GetProduct(string toCase)
+        {
+            switch (toCase.ToLower())
+            {
+                case "lower":
+                    return new ConcreteProductToLowerCase();
+                case "upper":
+                    return new ConcreteProductToUpperCase();
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+
+    class ConcreateCreatorReplaceFactory : SuperFactory
+    {
+        public override Product GetProduct(string toReplace)
+        {
+            switch (toReplace.ToLower())
+            {
+                case "comma":
+                    return new ConcreteProductSpaceToComma();
+                case "semicolon":
+                    return new ConcreteProductSpaceToSemicolon();
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+}
