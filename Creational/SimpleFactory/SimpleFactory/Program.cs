245fa084d61aa203ef1b69b48eddc8e83b967dcd
Matt <mathew.beldon@hotmail.co.uk>
2022-04-07 19:45:33 +0100

    [+] Simple Factory pattern

---
+++ Creational/SimpleFactory/SimpleFactory/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleFactory
+{
+    class Program
+    {
+        static void Main()
+        {
+            var productForUpper = SimpleFactory.GetProduct("upper");
+            var productForLower = SimpleFactory.GetProduct("lower");
+            var productForComma = SimpleFactory.GetProduct("comma");
+            var productForSemicolon = SimpleFactory.GetProduct("semicolon");
+
+            Console.WriteLine(productForLower.DoStuff("TO LOWER"));
+            Console.WriteLine(productForUpper.DoStuff("to upper"));
+            Console.WriteLine(productForComma.DoStuff("I want commas"));
+            Console.WriteLine(productForSemicolon.DoStuff("I want semicolons"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Creational/SimpleFactory/SimpleFactory/TheSimpleFactory.cs
@@ -0,0 +1,59 @@
+﻿namespace SimpleFactory
+{
+    abstract class Product
+    {
+        public abstract string DoStuff(string stuff);
+    }
+
+    class ConcreteProductToLowerCase : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.ToLower();
+        }
+    }
+
+    class ConcreteProductToUpperCase : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.ToUpper();
+        }
+    }
+
+    class ConcreteProductSpaceToComma : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.Replace(" ", ",");
+        }
+    }
+
+    class ConcreteProductSpaceToSemicolon : Product
+    {
+        public override string DoStuff(string stuff)
+        {
+            return stuff.Replace(" ", ";");
+        }
+    }
+
+    abstract class SimpleFactory
+    {
+        public static Product GetProduct(string productType)
+        {
+            switch (productType.ToLower())
+            {
+                case "lower":
+                    return new ConcreteProductToLowerCase();
+                case "upper":
+                    return new ConcreteProductToUpperCase();
+                case "comma":
+                    return new ConcreteProductSpaceToComma();
+                case "semicolon":
+                    return new ConcreteProductSpaceToSemicolon();
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+}
