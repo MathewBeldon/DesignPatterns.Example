cc44f95f0c4ac0ed4d259324b23732bb0932eed3
Matt <mathew.beldon@hotmail.co.uk>
2022-04-09 22:33:07 +0100

    [+] Builder pattern

---
+++ Creational/Builder/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Builder
+{
+    class Program
+    {
+        static void Main()
+        {
+            ProductMaker productMaker = new ProductMaker("Text to transform LOWER upper");
+
+            var toLowerAndCommaBuilder = new ToLowerAndCommaBuilder();
+            var lowerCommaProduct = productMaker.Make(toLowerAndCommaBuilder);
+            lowerCommaProduct.ShowTransforms();
+
+            Console.WriteLine();
+
+            var toUpperAndSemicolonBuilder = new ToUpperAndSemicolonBuilder();
+            var upperSemicolonProduct = productMaker.Make(toUpperAndSemicolonBuilder);
+            upperSemicolonProduct.ShowTransforms();
+
+            Console.ReadKey();
+        }
+    }   
+}
---
+++ Creational/Builder/TheBuilder.cs
@@ -0,0 +1,82 @@
+﻿namespace Builder
+{
+    public class Product
+    {
+        public string OriginalText { get; set; }
+        public string? CaseChangedText { get; set; }
+        public string? ReplaceText { get; set; }
+
+        public Product(string originalText)
+        {
+            OriginalText = originalText ?? throw new ArgumentNullException(nameof(originalText));
+        }
+
+        public void ShowTransforms()
+        {
+            Console.WriteLine("Orignal Text: " + OriginalText);
+            Console.WriteLine("Case Changed Text: " + CaseChangedText);
+            Console.WriteLine("Replaced Text: " + ReplaceText);
+        }
+    }
+
+    public abstract class TheBuilder
+    {
+        protected Product _product;
+
+        public abstract void CaseChangeText();
+        public abstract void ReplaceText();
+
+        public void CreateProduct(string originalText)
+        {
+            _product = new Product(originalText);
+        }
+        public Product GetProduct()
+        {
+            return _product;
+        }
+    }
+
+    class ToLowerAndCommaBuilder : TheBuilder
+    {
+        public override void CaseChangeText()
+        {
+            _product.CaseChangedText = _product.OriginalText.ToLower();
+        }
+
+        public override void ReplaceText()
+        {
+            _product.ReplaceText = _product.OriginalText.Replace(" ", ",");
+        }
+    }
+
+    class ToUpperAndSemicolonBuilder : TheBuilder
+    {
+        public override void CaseChangeText()
+        {
+            _product.CaseChangedText = _product.OriginalText.ToUpper();
+        }
+
+        public override void ReplaceText()
+        {
+            _product.ReplaceText = _product.OriginalText.Replace(" ", ";");
+        }
+    }
+
+    public class ProductMaker
+    {
+        private string _originalText;
+        public ProductMaker(string originalText)
+        {
+            _originalText = originalText;
+        }
+
+        public Product Make(TheBuilder theBuilder)
+        {
+            theBuilder.CreateProduct(_originalText);
+            theBuilder.CaseChangeText();
+            theBuilder.ReplaceText();
+
+            return theBuilder.GetProduct();
+        }
+    }
+}
