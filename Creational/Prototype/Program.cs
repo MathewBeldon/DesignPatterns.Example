6160a3868a82217631aae21528268bffa8a07ae3
Matt <mathew.beldon@hotmail.co.uk>
2022-04-12 20:37:50 +0100

    [+] Prototype pattern

---
+++ Creational/Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Prototype
+{
+    class Program
+    {
+        static void Main()
+        {
+            PixelColour pixelColourOne = new PixelColour(233, 32, 42, 255);
+            PixelColour pixelColourTwo = pixelColourOne.GetClone(); //traditional clone
+            PixelColour pixelColourThree = pixelColourOne with { }; //new record clone
+            pixelColourTwo.Green = 222;
+            pixelColourThree.Blue = 111;
+
+            Console.WriteLine(pixelColourOne.ToString());
+            Console.WriteLine(pixelColourTwo.ToString());
+            Console.WriteLine(pixelColourThree.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Creational/Prototype/ThePrototype.cs
@@ -0,0 +1,28 @@
+﻿namespace Prototype
+{
+    abstract record ThePrototype
+    {
+        public abstract ThePrototype GetClone();
+    }
+
+    record PixelColour : ThePrototype
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+        public int Alpha;
+
+        public PixelColour(int red, int green, int blue, int alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        public override PixelColour GetClone()
+        {
+            return MemberwiseClone() as PixelColour ?? throw new NullReferenceException(nameof(PixelColour));
+        }
+    }
+}
