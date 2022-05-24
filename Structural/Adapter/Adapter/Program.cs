fc9260f11afeedba5e7a347c40ea62f4e7b06a61
Matt <mathew.beldon@hotmail.co.uk>
2022-05-24 19:35:30 +0100

    [+] Adapter pattern

---
+++ Structural/Adapter/Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Adapter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string textToTransform = "TrAnSForM ThiS TExT To LoWeR";
+            IAdapter target = new Adapter();
+            var transformedText = target.Request(textToTransform);
+
+            Console.WriteLine(textToTransform);
+            Console.WriteLine(transformedText);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structural/Adapter/Adapter/TheAdapter.cs
@@ -0,0 +1,24 @@
+﻿namespace Adapter
+{
+    internal interface IAdapter
+    {
+        string Request(string text);
+    }
+
+    internal sealed class Adapter : IAdapter
+    {
+        private Adaptee adaptee = new Adaptee();
+        public string Request(string text)
+        {
+            return adaptee.TransformText(text);
+        }
+    }
+
+    internal sealed class Adaptee
+    {
+        public string TransformText(string text)
+        {
+            return text.ToLower();
+        }
+    }
+}
