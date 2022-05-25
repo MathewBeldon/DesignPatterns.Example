bf1a553ebae21eaf10c9a51f9e899c3b6ebda2b9
Matt <mathew.beldon@hotmail.co.uk>
2022-05-25 21:01:02 +0100

    [+] Bridge pattern

---
+++ Structural/Bridge/Bridge/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Bridge
+{
+    public class Program
+    {
+        const string TEXT_TO_TRANSFORM = "This Text To Transform";
+
+        public static void Main()
+        {
+            Abstraction transformer = new RefinedAbstraction();
+
+            transformer.Implementor = new ToLowerImplementor();
+            Console.WriteLine("    To Lower: " + transformer.Transform(TEXT_TO_TRANSFORM));
+
+            transformer.Implementor = new ToUpperImplementor();
+            Console.WriteLine("    To Upper: " + transformer.Transform(TEXT_TO_TRANSFORM));
+
+            transformer.Implementor = new RemoveSpaceImplementor();
+            Console.WriteLine("Remove Space: " + transformer.Transform(TEXT_TO_TRANSFORM));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structural/Bridge/Bridge/TheBridge.cs
@@ -0,0 +1,52 @@
+﻿namespace Bridge
+{
+    internal class Abstraction
+    {
+        protected IImplementor implementor;
+        public IImplementor Implementor
+        {
+            set { implementor = value; }
+        }
+        public virtual string Transform(string text)
+        {
+            return implementor.Transform(text);
+        }
+    }
+
+    internal interface IImplementor
+    {
+        string Transform(string text);
+    }
+
+    internal sealed class RefinedAbstraction : Abstraction
+    {
+        public override string Transform(string text)
+        {
+            return implementor.Transform(text);
+        }
+    }
+
+    internal sealed class ToLowerImplementor : IImplementor
+    {
+        public string Transform(string text)
+        {
+            return text.ToLower();
+        }
+    }
+
+    internal sealed class ToUpperImplementor : IImplementor
+    {
+        public string Transform(string text)
+        {
+            return text.ToUpper();
+        }
+    }
+
+    internal sealed class RemoveSpaceImplementor : IImplementor
+    {
+        public string Transform(string text)
+        {
+            return text.Replace(" ", string.Empty);
+        }
+    }
+}
