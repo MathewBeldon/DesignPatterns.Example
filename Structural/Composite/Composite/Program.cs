7da39520fbbd885901b4dad03de9175f0410568d
Matt <mathew.beldon@hotmail.co.uk>
2022-05-26 21:53:58 +0100

    [+] Composite pattern

---
+++ Structural/Composite/Composite/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Composite
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Folder root = new Folder("root");
+            root.Add(new File("index.html"));
+            root.Add(new File("style.css"));
+
+            Folder src = new Folder("src");
+            src.Add(new File("program.cs"));
+            src.Add(new File("startup.cs"));
+            root.Add(src);
+
+            Folder controller = new Folder("controllers");
+            controller.Add(new File("home_controller.cs"));
+            File accountController = new File("account_controller.cs");
+            controller.Add(accountController);
+
+            src.Add(controller);            
+            root.Display();
+
+            Console.WriteLine("\nDELETE `account_controllers.cs` from `controllers` \n");
+
+            controller.Remove(accountController);
+            root.Display();
+
+            Console.WriteLine("\nDELETE `controllers` \n");
+
+            src.Remove(controller);
+            root.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structural/Composite/Composite/TheComposite.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+
+    public abstract class Component
+    {
+        protected string name;
+
+        public Component(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Add(Component c);
+        public abstract void Remove(Component c);
+        public abstract void Display(int level = default);
+
+        protected void CustomWriteLine(int level, string name, char delimiter)
+        {
+            Console.WriteLine($"{new string(' ', level)}{delimiter} {name}");
+        }
+    }
+
+    public class Folder : Component
+    {
+        List<Component> children = new List<Component>();
+
+        public Folder(string name) : base(name){}
+
+        public override void Add(Component component)
+        {
+            children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            children.Remove(component);
+        }
+
+        public override void Display(int level = default)
+        {
+            CustomWriteLine(level, name, '-');
+
+            foreach (Component component in children)
+            {
+                component.Display(level + 1);
+            }
+        }
+    }
+
+    public class File : Component
+    {
+        public File(string name) : base(name){}
+
+        public override void Add(Component c)
+        {
+            throw new InvalidOperationException("Cannot add to a file");
+        }
+
+        public override void Remove(Component c)
+        {
+            throw new InvalidOperationException("Cannot remove from a file");
+        }
+
+        public override void Display(int level = default)
+        {
+            CustomWriteLine(level, name, '*');
+        }
+    }    
+}
