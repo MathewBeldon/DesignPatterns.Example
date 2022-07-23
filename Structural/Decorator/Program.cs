ba9c2071e44705c04f568b20b7d52d034abe1695
Matt <mathew.beldon@hotmail.co.uk>
2022-07-23 20:26:48 +0100

    [+] Decorator pattern

---
+++ Structural/Decorator/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Decorator
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string input = ",,,,, + test string + \"\"\"\"\"\"";
+
+            Sanitise sanitise = new Sanitiser();
+            SanitiseDecorator removeCommaDecorator = new RemoveCommaDecorator();
+            SanitiseDecorator removeQuoteDecorator = new RemoveQuoteDecorator();
+            SanitiseDecorator removeSpaceDecorator = new RemoveSpaceDecorator();
+            SanitiseDecorator removePlusDecorator = new RemovePlusDecorator();
+
+            string s1 = sanitise.Transform(input);
+            Console.WriteLine($"Default string: {s1}");
+
+            removeCommaDecorator.SetSanitiser(sanitise);
+            string t1  = removeCommaDecorator.Transform(input);
+            Console.WriteLine($"Removed commas: {t1}");
+
+            removeQuoteDecorator.SetSanitiser(sanitise);
+            string t2 = removeQuoteDecorator.Transform(input);
+            Console.WriteLine($"Removed quotes: {t2}");
+
+            removeSpaceDecorator.SetSanitiser(sanitise);
+            string t3 = removeSpaceDecorator.Transform(input);
+            Console.WriteLine($"Removed space: {t3}");
+
+            removePlusDecorator.SetSanitiser(sanitise);
+            string t4 = removePlusDecorator.Transform(input);
+            Console.WriteLine($"Removed plus: {t4}");
+
+            //long chain 
+            removeCommaDecorator.SetSanitiser(sanitise);
+            removeQuoteDecorator.SetSanitiser(removeCommaDecorator);
+            removeSpaceDecorator.SetSanitiser(removeQuoteDecorator);
+            removePlusDecorator.SetSanitiser(removeSpaceDecorator);
+            string t5 = removePlusDecorator.Transform(input);
+            Console.WriteLine($"Removed all: {t5}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structural/Decorator/TheDecorator.cs
@@ -0,0 +1,62 @@
+﻿namespace Decorator
+{
+    public interface Sanitise
+    {
+        string Transform(string input);
+    }
+
+    public class Sanitiser : Sanitise
+    {
+        public string Transform(string input)
+        {
+            return input;
+        }
+    }
+
+    public abstract class SanitiseDecorator : Sanitise
+    {
+        protected Sanitise sanitise;
+
+        public void SetSanitiser(Sanitise sanitise)
+        {
+            this.sanitise = sanitise;
+        }
+
+        public virtual string Transform(string input)
+        {
+            return sanitise.Transform(input);
+        }
+    }
+
+    public class RemoveCommaDecorator : SanitiseDecorator
+    {
+        public override string Transform(string input)
+        {
+            return base.Transform(input).Replace(",", string.Empty);
+        }
+    }
+
+    public class RemoveQuoteDecorator : SanitiseDecorator
+    {
+        public override string Transform(string input)
+        {
+            return base.Transform(input).Replace("\"", string.Empty);
+        }
+    }
+
+    public class RemoveSpaceDecorator : SanitiseDecorator
+    {
+        public override string Transform(string input)
+        {
+            return base.Transform(input).Replace(" ", string.Empty);
+        }
+    }
+
+    public class RemovePlusDecorator : SanitiseDecorator
+    {
+        public override string Transform(string input)
+        {
+            return base.Transform(input).Replace("+", string.Empty);
+        }
+    }
+}
