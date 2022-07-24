12c0239a34966fd8e493de8a517f863e1c14ed66
Matt <mathew.beldon@hotmail.co.uk>
2022-07-24 19:21:34 +0100

    [+] Facade pattern

---
+++ Structural/Facade/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Facade
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string input = ",,,,, + test string + \"\"\"\"\"\"";
+
+            TextTransformer transformer = new TextTransformer();
+
+            string t1 = transformer.RemoveSpaceAndComma(input);
+            Console.WriteLine($"Removed space and comma: {t1}");
+
+            string t2 = transformer.RemovePlusAndQuote(input);
+            Console.WriteLine($"Removed plus and quote: {t2}");
+
+            string t3 = transformer.RemoveAll(input);
+            Console.WriteLine($"Removed all: {t3}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structural/Facade/TheFacade.cs
@@ -0,0 +1,70 @@
+﻿namespace Facade
+{
+    public class RemoveSpace
+    {
+        public string Transform(string input)
+        {
+            return input.Replace(" ", string.Empty);
+        }
+    }
+
+    public class RemoveComma
+    {
+        public string Transform(string input)
+        {
+            return input.Replace(",", string.Empty);
+        }
+    }
+
+    public class RemovePlus
+    {
+        public string Transform(string input)
+        {
+            return input.Replace("+", string.Empty);
+        }
+    }
+
+    public class RemoveQuote
+    {
+        public string Transform(string input)
+        {
+            return input.Replace("\"", string.Empty);
+        }
+    }
+
+    public class TextTransformer
+    {
+        RemoveSpace removeSpace;
+        RemoveComma removeComma;
+        RemovePlus removePlus;
+        RemoveQuote removeQuote;
+
+        public TextTransformer()
+        {
+            removeSpace = new RemoveSpace();
+            removeComma = new RemoveComma();
+            removePlus = new RemovePlus();
+            removeQuote = new RemoveQuote();
+        }
+
+        public string RemoveSpaceAndComma(string input)
+        {
+            string spaceRemoved = removeSpace.Transform(input);
+            return removeComma.Transform(spaceRemoved);
+        }
+
+        public string RemovePlusAndQuote(string input)
+        {
+            string plusRemoved = removePlus.Transform(input);
+            return removeQuote.Transform(plusRemoved);
+        }
+
+        public string RemoveAll(string input)
+        {
+            string spaceRemoved = removeSpace.Transform(input);
+            string commaRemoved = removeComma.Transform(spaceRemoved);
+            string plusRemoved = removePlus.Transform(commaRemoved);
+            return removeQuote.Transform(plusRemoved);
+        }
+    }
+}
