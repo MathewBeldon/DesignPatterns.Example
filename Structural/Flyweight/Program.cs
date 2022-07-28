b9a71c472d7ebf1946454956441179002dfe43ef
Matt <mathew.beldon@hotmail.co.uk>
2022-07-28 21:53:36 +0100

    [+] Flyweight pattern

---
+++ Structural/Flyweight/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Flyweight
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var flyweightFactory = new FlyweightFactory(
+                new Pixel { R = 22, G = 12, B = 33 },
+                new Pixel { R = 112, G = 1, B = 99 },
+                new Pixel { R = 42, G = 255, B = 123 },
+                new Pixel { R = 55, G = 0, B = 0 }
+            );
+
+            AddPixel(new Pixel { R = 22, G = 12, B = 33, PositionX = 1, PositionY = 3 });
+            AddPixel(new Pixel { R = 22, G = 12, B = 33, PositionX = 1, PositionY = 4 });
+            AddPixel(new Pixel { R = 112, G = 1, B = 99, PositionX = 444, PositionY = 555 });
+            AddPixel(new Pixel { R = 55, G = 0, B = 0, PositionX = 222, PositionY = 23 });
+            AddPixel(new Pixel { R = 42, G = 255, B = 123, PositionX = 1233, PositionY = 3321 });
+            AddPixel(new Pixel { R = 255, G = 255, B = 255, PositionX = 999, PositionY = 999 });
+
+            flyweightFactory.DisplayFlyweights();
+
+            Console.ReadKey();
+
+            void AddPixel(Pixel pixel)
+            {
+                var flyweight = flyweightFactory.GetFlyweight(new Pixel
+                {
+                    R = pixel.R,
+                    G = pixel.G,
+                    B = pixel.B
+                });
+
+                flyweight.Operation(pixel);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Structural/Flyweight/TheFlyweight.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.Json;
+
+namespace Flyweight
+{
+    public class FlyweightFactory
+    {
+        private Dictionary<string, Flyweight> flyweights { get; set; } = new Dictionary<string, Flyweight>();
+
+        public FlyweightFactory(params Pixel[] pixels)
+        {
+            foreach (var pixel in pixels)
+            {
+                flyweights.Add(pixel.GetHash(), new Flyweight(pixel));
+            }
+        }
+
+        public Flyweight GetFlyweight(Pixel pixel)
+        {
+            if (flyweights.ContainsKey(pixel.GetHash()))
+            {
+                Console.WriteLine("Reusing flyweight");
+                return flyweights[pixel.GetHash()];
+            } 
+            else
+            {
+                Console.WriteLine("Creating new flyweight");
+                var flyweight = new Flyweight(pixel);
+                flyweights.Add(pixel.GetHash(), flyweight);
+                return flyweight;
+            }
+        }
+
+        public void DisplayFlyweights()
+        {
+            Console.WriteLine($"Count: {flyweights.Count}");
+            foreach (var flyweight in flyweights)
+            {
+                Console.WriteLine($"Hash: {flyweight.Key}");
+            }
+        }
+    }
+
+    public class Flyweight
+    {
+        private Pixel _sharedState;
+
+        public Flyweight(Pixel pixel)
+        {
+            _sharedState = pixel;
+        }
+
+        public void Operation(Pixel uniquePixel)
+        {
+            string state = JsonSerializer.Serialize(_sharedState);
+            string unique = JsonSerializer.Serialize(uniquePixel);
+            Console.WriteLine($"shared state {state} unique state {unique}");
+        }
+    }
+
+    public class Pixel
+    {
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+
+        public string GetHash()
+        {
+            return $"{R}-{G}-{B}";
+        }
+    }
+}
