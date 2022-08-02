6500c815d7cc99b26c829cd9ef5ad491b36a5f80
Matt <mathew.beldon@hotmail.co.uk>
2022-08-02 21:19:45 +0100

    [+] Proxy pattern

---
+++ Structural/Proxy/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Proxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TransformProxy proxy = new TransformProxy();
+
+            const string input = "This Is A String, This Is A String";
+
+            Console.WriteLine($"Removed space: {proxy.RemoveSpace(input)}");
+            Console.WriteLine($"Removed comma: {proxy.RemoveComma(input)}");
+            Console.WriteLine($"To lowercase: {proxy.ToLower(input)}");
+            Console.WriteLine($"To uppercase: {proxy.ToUpper(input)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structural/Proxy/TheProxy.cs
@@ -0,0 +1,57 @@
+﻿namespace Proxy
+{
+    public interface ITransform
+    {
+        string RemoveSpace(string input);
+        string RemoveComma(string input);
+        string ToUpper(string input);
+        string ToLower(string input);
+    }
+
+    public class Transform : ITransform
+    {
+        public string RemoveSpace(string input)
+        {
+            return input.Replace(" ", string.Empty);
+        }
+
+        public string RemoveComma(string input)
+        {
+            return input.Replace(",", string.Empty);
+        }
+
+        public string ToUpper(string input)
+        {
+            return input.ToUpper();
+        }
+
+        public string ToLower(string input)
+        {
+            return input.ToLower();
+        }
+    }
+
+    public class TransformProxy : ITransform
+    {
+        private Transform _transform = new Transform();
+        public string RemoveSpace(string input)
+        {
+            return _transform.RemoveSpace(input);
+        }
+
+        public string RemoveComma(string input)
+        {
+            return _transform.RemoveComma(input);
+        }
+
+        public string ToUpper(string input)
+        {
+            return _transform.ToUpper(input);
+        }
+
+        public string ToLower(string input)
+        {
+            return _transform.ToLower(input);
+        }
+    }
+}
