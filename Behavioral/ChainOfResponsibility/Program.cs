9afb12244613a95de5352abaae7d2b093780fdfe
Matt <mathew.beldon@hotmail.co.uk>
2022-08-03 19:08:34 +0100

    [+] Chain of responsibility pattern

---
+++ Behavioral/ChainOfResponsibility/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ChainOfResponsibility
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Setup Chain of Responsibility
+            PermissionChecker adminChecker = new AdminChecker();
+            PermissionChecker memberChecker = new MemberChecker();
+            PermissionChecker guestChecker = new GuestChecker();
+
+            adminChecker.SetSuccessor(memberChecker);
+            memberChecker.SetSuccessor(guestChecker);
+
+            var adminUser = new User()
+            {
+                Name = "Alice",
+                Role = UserRole.Admin
+            };
+            adminChecker.HandleRequest(adminUser);
+
+            var memberUser = new User()
+            {
+                Name = "Bob",
+                Role = UserRole.Member
+            };
+            adminChecker.HandleRequest(memberUser);
+
+            var guestUser = new User()
+            {
+                Name = "Anonymous",
+                Role = UserRole.Guest
+            };
+            adminChecker.HandleRequest(guestUser);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Behavioral/ChainOfResponsibility/TheChainOfResponsibility.cs
@@ -0,0 +1,69 @@
+﻿namespace ChainOfResponsibility
+{
+    public abstract class PermissionChecker
+    {
+        protected PermissionChecker successor;
+        public void SetSuccessor(PermissionChecker successor)
+        {
+            this.successor = successor;
+        }
+        public abstract void HandleRequest(User user);
+    }
+
+    public class AdminChecker : PermissionChecker
+    {
+        public override void HandleRequest(User user)
+        {
+            if (UserRole.Admin == user.Role)
+            {
+                Console.WriteLine("User: {0} has the role Admin", user.Name);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(user);
+            }
+        }
+    }
+
+    public class MemberChecker : PermissionChecker
+    {
+        public override void HandleRequest(User user)
+        {
+            if (UserRole.Member == user.Role)
+            {
+                Console.WriteLine("User: {0} has the role Member", user.Name);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(user);
+            }
+        }
+    }
+
+    public class GuestChecker : PermissionChecker
+    {
+        public override void HandleRequest(User user)
+        {
+            if (UserRole.Guest == user.Role)
+            {
+                Console.WriteLine("User: {0} has the role Guest", user.Name);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(user);
+            }
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public UserRole Role { get; set; }
+    }
+
+    public enum UserRole {
+        Admin,
+        Member, 
+        Guest
+    }
+}
